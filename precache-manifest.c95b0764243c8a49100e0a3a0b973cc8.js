1f97206e1986735c10b903251fd9898457e7e38e
13jun <820373584@qq.com>
2021-08-09 13:13:46 +0800

    deploy

---
+++ precache-manifest.c95b0764243c8a49100e0a3a0b973cc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57f203d159ac0aac9f07",
+    "url": "/sun-money-dev/css/app.456f5a70.css"
+  },
+  {
+    "revision": "44b47386d263e025c69d9a3b5a3738b8",
+    "url": "/sun-money-dev/index.html"
+  },
+  {
+    "revision": "57f203d159ac0aac9f07",
+    "url": "/sun-money-dev/js/app.34f05fb5.js"
+  },
+  {
+    "revision": "d1925816957868c43521",
+    "url": "/sun-money-dev/js/chunk-vendors.b50a5a37.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/sun-money-dev/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sun-money-dev/robots.txt"
+  }
+]);
